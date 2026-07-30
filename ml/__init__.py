d1921879859fccb81baa66ef372679995a89ad18
adanjoserrojas <adanrojas224@gmail.com>
2026-07-29 23:12:02 -0400

    Some nonsense brah

---
+++ ml/__init__.py
@@ -0,0 +1,4 @@
+__version__ = "0.1.0"
+__author__ = "My Mom"
+__license__ = "Use it bro, I like the gym, I bet you too"
+__all__ = [""] # we shall see what becomes usable... Even though, I do not really care lol
---
+++ ml/repository.py
@@ -0,0 +1,7 @@
+"""
+-======================================
+Tonight I am literally spending my entire night setting up my entire workstation to use the AWS CLI
+and the AWS CDK, do not judge my commits today lol
+
+It's honestly a lot of docs lol
+"""
