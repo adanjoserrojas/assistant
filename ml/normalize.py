504824148328e0bd86af6162d68709e30cb32aef
adanjoserrojas <adanrojas224@gmail.com>
2026-08-02 02:05:41 -0400

    Fixed: Import and building protos to normalize data received from DynamoDB

--- ml/normalize.py
+++ ml/normalize.py
@@ -1,11 +1,26 @@
+import json
 from boto3.dynamodb.types import TypeDeserializer
-from repository import (
+from ml.repository import (
     fetch_sessions as logs,
     count_training_sessions as amount
 )
+from datetime import datetime
 
 '''
 Sole purpose of this thing is to clean and prepare daata for ML logistics regression model
 And mean calculation
 '''
 
+# protos
+
+def good_boy() -> int:
+    # print(json.dumps(logs(), indent=2, default=str)) I see what we are deling with, my son
+    return 1
+def parse_time() -> datetime:
+    pass
+def decimal_convert() -> int:
+    pass
+def build_training_data():
+    pass
+
+good_boy()
