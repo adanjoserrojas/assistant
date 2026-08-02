8bf882e04885b0835577ec1d04a02bbd4fc469dc
adanjoserrojas <adanrojas224@gmail.com>
2026-08-02 02:33:19 -0400

    Build_training_data completed, skeleton for the data the logistics regression model eats is done, I am gonna go to lseep

--- ml/normalize.py
+++ ml/normalize.py
@@ -13,14 +13,35 @@ And mean calculation
 
 # protos
 
-def good_boy() -> int:
-    # print(json.dumps(logs(), indent=2, default=str)) I see what we are deling with, my son
-    return 1
-def parse_time() -> datetime:
-    pass
+def good_boy() -> list[dict]:
+    data = logs()
+    print(json.dumps(data, indent=2, default=str)) # I see what we are deling with, my son
+    return data
+
+def parse_time(old: dict):
+    date: datetime = None
+    weekday: str = ""
+
+    return [date, weekday]
+
 def decimal_convert() -> int:
     pass
-def build_training_data():
-    pass
 
+def build_training_data(old_data: list[dict]) -> list[dict]:
+
+    new_data = []
+
+    for item in old_data:
+        
+        started_at, weekday = parse_time(item)
+        new_data.append({
+            "workout": item["workout"],
+            "duration": decimal_convert(item["actual_duration_minutes"]),
+            "location": item["location_code"],
+            "started_at": started_at,
+            "weekday": weekday
+        })
+
+    return new_data
+        
 good_boy()
