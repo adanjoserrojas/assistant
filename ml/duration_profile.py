a3f0c01f181dad06af7f5d5596e3d08c9927ef78
adanjoserrojas <adanrojas224@gmail.com>
2026-08-15 21:37:50 -0400

    Added: prototypes

--- ml/duration_profile.py
+++ ml/duration_profile.py
@@ -4,5 +4,17 @@ Read completed gym sessions from DynamoDB.
 Group them by workout.
 Calculate mean duration.
 Save the result to S3.
-
 '''
+from .normalize import (
+    build_training_data as eligible
+)
+
+def group_workout() -> list[dict]:
+    pass
+def calculate_mean() -> int:
+    pass
+def send_S3() -> None:
+    pass
+
+def main():
+    pass
