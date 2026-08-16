7e197117411fe0df47146d4c90990a8edb119c62
adanjoserrojas <adanrojas224@gmail.com>
2026-08-16 02:50:03 -0400

    Modified: Calculate_mean method

--- ml/duration_profile.py
+++ ml/duration_profile.py
@@ -17,18 +17,30 @@ def group_workout(data: list[dict]) -> dict[str, list[dict]]:
 
     for item in data:
 
-        workout_type = item.pop("workout", None)
-        res.setdefault(workout_type, []).append(item)
+        session = dict(item)
+        workout_type = session.pop("workout", None)
+        res.setdefault(workout_type, []).append(session)
 
     return res
 
 
-def calculate_mean() -> int:
-    pass
+def calculate_mean() -> dict[str, float]:
+
+    grouped_data = group_workout(CLEAN_DATA)
+    res: dict[str, float] = {}
+
+    for workout_type, sessions in grouped_data.items():
+        total = sum(session["duration"] for session in sessions)
+        res[workout_type] = round((total / len(sessions)), 2)
+
+    return res
+
 def send_S3() -> None:
     pass
 
 def main():
     print(json.dumps(group_workout(CLEAN_DATA), indent=4))
+    print("----------------------------------------------")
+    print(json.dumps(calculate_mean(), indent=4))
 
 main()
